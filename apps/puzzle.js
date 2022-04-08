47720e10179099e1c967ac887d22c073d9022197
Zimo Zhai <mapmaths@outlook.com>
2022-04-08 12:48:06 +0800

    environment set-ups

---
+++ apps/puzzle.js
@@ -0,0 +1,8 @@
+/**
+ * @file apps/puzzle.js
+ * @copyright Copyright (c) 2022 MapMaths
+ * @license MIT
+ * @author MapMaths
+ * @repo https://GitHub.com/MapMaths/plcx
+ * @description Automatically change a PNG file to RGB LED pixels
+ */
---
+++ savsetup.js
@@ -0,0 +1,12 @@
+/**
+ * @file savsetup.js
+ * @copyright Copyright (c) 2022 MapMaths
+ * @license MIT
+ * @author MapMaths
+ * @repo https://GitHub.com/MapMaths/plcx
+ * @description Setting up environment for JSON files.
+ */
+
+function setup0() {
+
+}
