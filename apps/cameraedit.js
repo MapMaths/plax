e5ac62302400a289b491fba81fa1b9dfebc59ff3
Zimo Zhai <mapmaths@outlook.com>
2022-04-15 20:42:08 +0800

    Very first version

---
+++ apps/cameraedit.js
@@ -0,0 +1,16 @@
+/**
+ * @file apps/edit.js
+ * @copyright Copyright (c) 2022 MapMaths
+ * @license MIT
+ * @author MapMaths
+ * @repo https://GitHub.com/MapMaths/plcx
+ * @description A short app to read and write a PLC local file.
+ */
+
+const plcx = require("../savedit.js"),
+    filename = "C:\\Users\\Administrator\\AppData\\LocalLow\\CIVITAS\\Quantum Physics\\Circuit\\307e56b8-2f9a-4d11-bb29-f227b32e2eee.sav";
+var json = plcx.read(filename);
+
+json = plcx.setCamera(json, null, null, [0, 4, 0], null);
+console.log(json);
+plcx.write(filename, json)
--- savedit.js
+++ savedit.js
@@ -5,11 +5,11 @@
  * @author MapMaths
  * @repo https://GitHub.com/MapMaths/plcx
  * @description Edit present JSON files.
+ * @en Node.js
  */
 
 "use strict";
 
-//import { readFile, writeFile } from 'fs';
 const fs = require("fs");
 
 /**
@@ -29,6 +29,26 @@ function write(filename, json) {
     fs.writeFileSync(filename, JSON.stringify(json, null, 2));
 }
 
+/**
+ * @function Modify camera settings.
+ * @param {string} json The JSON string to be modified.
+ * @param {int} mode 0 = Electricity, 1 = Graphical electricity, 3 = Universe, 4 = Electromagnetic field.
+ * @param {digital} dist The distance of the camera to the sight center.
+ * @param {array[float]} pos The position of the camera, at an index of [x, y, z].
+ * @param {array[float]} rot The rotation of the camera, at an index of [xy, xz, yz].
+ */
+function setCamera(json, mode, dist, pos, rot) {
+    let origin = json;
+    let modify = JSON.parse(origin.Experiment.CameraSave)
+    if (mode != null) modify.Mode = mode;
+    if (dist != null) modify.Distance = dist;
+    if (pos != null) modify.VisionCenter = `${pos[0]},${pos[2]},${pos[1]}`;
+    if (rot != null) modify.TargetRotation = `${rot[0]},${rot[2]},${rot[1]}`;
+    origin.Experiment.CameraSave = JSON.stringify(modify);
+    return origin;
+}
+
 // Export the functions.
 exports.read = read;
 exports.write = write;
+exports.setCamera = setCamera;
