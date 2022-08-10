484b34030aa823fa4abc6f4405866386c0751075
MapMaths <mapmaths@outlook.com>
2022-08-10 10:12:07 +0800

    Delete puzzle.js

--- demos/puzzle.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file apps/puzzle.js
- * @copyright Copyright (c) 2022 MapMaths
- * @license MIT
- * @author MapMaths
- * @repo https://GitHub.com/MapMaths/plcx
- * @description Automatically change a PNG file to RGB LED pixels
- */
