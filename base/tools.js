d3ac1718615f495673b0ae4064a6f155460390a5
MapMaths <mapmaths@outlook.com>
2022-08-11 10:45:15 +0800

    Create tools.js

---
+++ base/tools.js
@@ -0,0 +1,39 @@
+/**
+ * @file tools.js
+ * @copyright Copyright (c) 2022 MapMaths
+ * @license MIT
+ * @author MapMaths
+ * @repo https://GitHub.com/MapMaths/plcx
+ * @description Setting up environment for JSON files.
+ */
+
+randomID(length) {
+  // Generate a random HEX ID, by the given length.
+  let id = "";
+  for (let i = 0; i < length; i++) {
+    let rand = Math.floor(Math.random() * 16);
+    let n = rand < 10 ? 48 : 87;
+    id += String.fromCharCode(n + rand);
+  }
+  return id;
+}
+
+generateNewElementID(json) {
+  let elements = JSON.parse(json.StatusSave).Elements;
+  let repeated = true;
+  let id = '';
+  while (repeated) {
+    repeated = false;
+    id = randomID(32);
+    // Is it a repeated ID?
+    for (let i = 0; i < elements.length; i++) {
+      if (!repeated) repeated = false;
+      if (elements[i].Identifier == id) repeated = true;
+    }
+  }
+  return id;
+}
+
+// Export the functions.
+exports.randomID = randomID;
+exports.generateNewElementID = generateNewElementID;
